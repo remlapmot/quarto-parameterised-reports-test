8ac16d93c7efc23dd150dfb858e44fd568db0a07
Tom Palmer <remlapmot@hotmail.com>
2025-02-10 10:22:00 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+uv python install 3.13 --preview
 uv venv --python 3.13
+
+# For nbstata
+# In Stata: python set exec ~/.local/bin/python3, perm
 uv pip install nbstata jupyterlab_stata_highlight2
-# julia --project=@quarto -e 'import Pkg; Pkg.add("QuartoNotebookRunner")'
+# Not used: julia --project=@quarto -e 'import Pkg; Pkg.add("QuartoNotebookRunner")'
 # Install IJulia
 # https://github.com/JuliaLang/IJulia.jl
