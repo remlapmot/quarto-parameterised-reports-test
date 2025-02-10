55f4fd38e2218e31b736a69f6e772e9de96be2dc
Tom Palmer <remlapmot@hotmail.com>
2025-02-10 10:05:08 +0000

    Add IJulia comments

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 uv venv --python 3.13
 uv pip install nbstata jupyterlab_stata_highlight2
-julia --project=@quarto -e 'import Pkg; Pkg.add("QuartoNotebookRunner")'
+# julia --project=@quarto -e 'import Pkg; Pkg.add("QuartoNotebookRunner")'
+# Install IJulia
+# https://github.com/JuliaLang/IJulia.jl
