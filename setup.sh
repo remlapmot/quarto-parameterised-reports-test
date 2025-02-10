82dd069fadedfe2f0eeac2009c97e751c81ba367
Tom Palmer <remlapmot@hotmail.com>
2025-02-10 10:22:22 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 uv python install 3.13 --preview
 uv venv --python 3.13
 
+source .venv/bin/activate
+
 # For nbstata
 # In Stata: python set exec ~/.local/bin/python3, perm
 uv pip install nbstata jupyterlab_stata_highlight2
