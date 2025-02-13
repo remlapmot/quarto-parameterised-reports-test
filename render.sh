82e12c970e3cd3c4135ee68988dfbb50edeb7568
Tom Palmer <remlapmot@hotmail.com>
2025-02-13 09:54:06 +0000

    Update render.sh

--- render.sh
+++ render.sh
@@ -5,3 +5,6 @@ quarto render nbstata-example.qmd --execute
 quarto render julia-example.qmd --execute
 
 quarto render julia-example-2.qmd --execute
+
+quarto render _stata-code-test.qmd --execute
+quarto render child-doc-test.qmd
