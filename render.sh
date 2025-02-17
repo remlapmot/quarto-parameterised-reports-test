29d4512a5513c6396b28293b3513d3f1a3319568
Tom Palmer <remlapmot@hotmail.com>
2025-02-17 09:53:49 +0000

    Update render.sh

--- render.sh
+++ render.sh
@@ -19,5 +19,6 @@ quarto render contents-test-2.qmd
 quarto render nbstata-example-2.qmd --execute
 
 HIDE_ANSWERS=1 quarto render nbstata-example-3.qmd --execute
+HIDE_ANSWERS=0 quarto render nbstata-example-3.qmd -o nbstata-example-3-false.html --execute
 
 HIDE_ANSWERS=true quarto render julia-example-3.qmd --execute
