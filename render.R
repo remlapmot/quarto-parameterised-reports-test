93979651f1561dffd1cd15386f571c22999c3ede
Tom Palmer <remlapmot@hotmail.com>
2025-02-18 11:38:06 +0000

    Update render.R

--- render.R
+++ render.R
@@ -1,1 +1,10 @@
 quarto::quarto_render("discussion-12026-reply.qmd")
+
+rmarkdown::render("exercise.Rmd",
+  output_file = "exercise-questions",
+  params = list(solutions = FALSE, title = "Example exercise: Questions")
+)
+
+rmarkdown::render("exercise.Rmd",
+  output_file = "exercise-solutions"
+)
