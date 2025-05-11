b97a8e091b19f94dc83dad4cd1fe0b0acd7d8b28
Tom Palmer <remlapmot@hotmail.com>
2025-05-11 06:00:12 +0100

    Delete old command

--- render.R
+++ render.R
@@ -1,5 +1,3 @@
-quarto::quarto_render("discussion-12026-reply.qmd")
-
 rmarkdown::render("exercise.Rmd",
   output_file = "exercise-questions",
   params = list(solutions = FALSE, title = "Example exercise: Questions")
