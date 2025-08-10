0f3f38732b6dd567097327616e70114f18783e77
Tom Palmer <remlapmot@hotmail.com>
2025-08-10 05:47:51 +0100

    Update render.sh

--- render.sh
+++ render.sh
@@ -14,3 +14,5 @@ quarto render exercise-julia.qmd -P hide_answers:false -o exercise-julia-solutio
 
 quarto render exercise-r-2.qmd -o exercise-r-2-questions.html
 quarto render exercise-r-2.qmd -P hide_answers:false -o exercise-r-2-solutions.html
+
+quarto render index.qmd
